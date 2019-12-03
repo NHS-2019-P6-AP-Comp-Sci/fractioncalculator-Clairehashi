6c3b8f54f5225f103bac2cb5283e73d1107d9286
s-hashizumec <s-hashizumec@53S50030963.it.bsd405.org>
2019-12-02 23:58:36 -0800

    checkpoint3

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -15,7 +15,7 @@ public class FracCalc {
 		String equation = userInput.nextLine();
 		while (!equation.equals("quit")) {
 			System.out.println(produceAnswer(equation));
-			equation = userInput.nextLine();
+			equation = userInput.nextLine();		
 		}
 
 	}
@@ -31,36 +31,74 @@ public class FracCalc {
 	// The function should return the result of the fraction after it has been
 	// calculated
 	// e.g. return ==> "1_1/4"
+
 	public static String produceAnswer(String input) {
 		// TODO: Implement this function to produce the solution to the input
 		int index = input.indexOf(" ");
 		String operand1 = input.substring(0, index);
 		String operator = input.substring(index + 1, index + 2);
 		String operand2 = input.substring(index + 3);
-		String answer = operand1 + operator + operand2;
-
+		
+		String op1Whol = Whole(operand1);
+		String op1Num = Num(operand1);
+		String op1Den = Den(operand1);
 		String op2Num = Num(operand2);
 		String op2Den = Den(operand2);
 		String op2Whol = Whole(operand2);
-
+		
+		int num1 = Integer.parseInt(Num(operand1));
+		int num2 = Integer.parseInt(Num(operand2));
+		int whole1 = Integer.parseInt(Whole(operand1));
+		int whole2 = Integer.parseInt(Whole(operand2));
+		int den1 = Integer.parseInt(Den(operand1));
+		int den2 = Integer.parseInt(Den(operand2));
+		int num3 = 0;
+		int den3 = 0;
+		int total = 0;
+		
 		String op2Value = "whole:" + op2Whol + " numerator:" + op2Num + " denominator:" + op2Den;
-		return op2Value;
+		
+		num1 = num1 + Math.abs(whole1) * den1;
+		num2 = num2 + Math.abs(whole2) * den2;
+		if (whole1 < 0){
+			num1 *= -1;
+		}
+		if (whole2 < 0){
+			num2 *= -1;
+		}
+		if (operator.equals("+")) {
+			num3 = (num1 * den2) + (num2 * den1);
+			den3 = (den1 * den2);
+		} else if (operator.equals("-")) {
+			num3 = (num1 * den2) - (num2 * den1);
+			den3= (den1 * den2);
+		} else if (operator.equals("*")){
+			num3 = (num1 * num2);
+			den3 = (den1 * den2);
+		}else if (operator.equals("/")) {
+			num3 = (num1 * den2);
+			den3 = (den1 * num2);
+		}
+		String result = (num3 + "/" + den3);
+		return result;
 	}
 
 	// TODO: Fill in the space below with any helper methods that you think you will
 	// need
+	// parses whole from user input
 	public static String Whole(String a) {
 		if (a.indexOf("_") != -1) {
-			// mixed 
+			// mixed
 			return a.substring(0, a.indexOf("_"));
 		} else if (a.indexOf("/") != -1) {
 			// impartial
-				return "0";
-			} else {
-				return a;
-			}
+			return "0";
+		} else {
+			return a;
 		}
+	}
 
+	// parses numerator from user input
 	public static String Num(String a) {
 		if (a.indexOf("_") != -1) {
 			// mixed fraction
@@ -74,6 +112,7 @@ public class FracCalc {
 		}
 	}
 
+	// parses denominator from user input
 	public static String Den(String a) {
 		if (a.indexOf("/") != -1) {
 			// mixed or impartial fraction
